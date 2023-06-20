9f3bfc870acc291add3e8a37dc03deb31b11567b
Podzorov Eugene <p.eugenics@gmail.com>
2023-06-20 14:53:47 +0700

    view model pallet error check

--- app/src/main/java/com/eugenics/freeradio/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/eugenics/freeradio/ui/viewmodels/MainViewModel.kt
@@ -138,11 +138,15 @@ class MainViewModel @Inject constructor(
             if (response.isSuccessful) {
                 response.body?.let { responseBody ->
                     val bitmap = BitmapFactory.decodeStream(responseBody.byteStream())
-                    Palette.from(bitmap)
-                        .generate()
-                        .dominantSwatch?.let {
-                            _primaryDynamicColor.value = it.rgb
-                        }
+                    try {
+                        Palette.from(bitmap)
+                            .generate()
+                            .dominantSwatch?.let {
+                                _primaryDynamicColor.value = it.rgb
+                            }
+                    } catch (e: Exception) {
+                        Log.e(TAG, e.toString())
+                    }
                 }
             }
         }
