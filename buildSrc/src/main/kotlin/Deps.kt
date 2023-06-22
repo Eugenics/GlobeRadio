cf09ce185525939bb9378dec2cb32e0fc38a480f
Podzorov Eugene <p.eugenics@gmail.com>
2023-06-22 11:32:29 +0700

    app gradle module

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -5,6 +5,11 @@ object BaseConfig {
     const val jvmTarget = "11"
     const val compileSdk = 33
     const val minSdk = 27
+    const val targetSdk = 33
+    const val applicationId = "com.eugenics.freeradio"
+    const val versionCode = 2
+    const val versionName = "1.1"
+    const val kotlinCompilerExtensionVersion = "1.4.7"
 }
 
 object Deps {
@@ -14,6 +19,42 @@ object Deps {
         const val coreKtx = "androidx.core:core-ktx:1.10.1"
         const val appcompat = "androidx.appcompat:appcompat:1.6.1"
         const val media = "androidx.media:media:1.6.0"
+        const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:2.6.1"
+        const val lifecycleViewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1"
+        const val activityCompose = "androidx.activity:activity-compose:1.7.2"
+        const val pallete = "androidx.palette:palette:1.0.0"
+        const val navigation = "androidx.navigation:navigation-compose:2.5.3"
+        const val testCore = "androidx.test:core:1.5.0"
+        const val testRunner = "androidx.test:runner:1.5.2"
+    }
+
+    object Compose {
+        private const val composeVersion = "1.4.3"
+        const val composeUi = "androidx.compose.ui:ui:$composeVersion"
+        const val composeTooling = "androidx.compose.ui:ui-tooling:$composeVersion"
+        const val composePreview = "androidx.compose.ui:ui-tooling-preview:$composeVersion"
+        const val animationGraphics =
+            "androidx.compose.animation:animation-graphics:$composeVersion"
+        const val uiTesting = "androidx.compose.ui:ui-test:$composeVersion"
+        const val jUnitTesting = "androidx.compose.ui:ui-test-junit4:$composeVersion"
+        const val uiTestManifest = "androidx.compose.ui:ui-test-manifest:$composeVersion"
+    }
+
+    object Accomponist {
+        const val inserts = "com.google.accompanist:accompanist-insets:0.25.1"
+        const val coil = "com.google.accompanist:accompanist-coil:0.15.0"
+        const val swipe = "com.google.accompanist:accompanist-swiperefresh:0.25.1"
+        const val systemUiController =
+            "com.google.accompanist:accompanist-systemuicontroller:0.28.0"
+        const val navigaionAnimation =
+            "com.google.accompanist:accompanist-navigation-animation:0.30.0"
+    }
+
+    object Material3 {
+        private const val material3Version = "1.1.1"
+        const val material = "androidx.compose.material3:material3:$material3Version"
+        const val windowSize =
+            "androidx.compose.material3:material3-window-size-class:$material3Version"
     }
 
     object JUnit {
@@ -30,6 +71,7 @@ object Deps {
         const val hilt = "com.google.dagger:hilt-android:$hiltVersion"
         const val compiler = "com.google.dagger:hilt-android-compiler:$hiltVersion"
         const val testing = "com.google.dagger:hilt-android-testing:$hiltVersion"
+        const val navigationCompose = "androidx.hilt:hilt-navigation-compose:1.0.0"
     }
 
     object Dagger {
@@ -59,11 +101,15 @@ object Deps {
             "org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion"
         const val coroutinesAndroid =
             "org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinCoroutinesVersion"
+
+        private const val kotlinVersion = "1.8.21"
+        const val kotlinStdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
     }
 
     object Google {
         private const val gsonVersion = "2.10.1"
         const val gson = "com.google.code.gson:gson:$gsonVersion"
+        const val material = "com.google.android.material:material:1.9.0"
     }
 
     object Retrofit {
@@ -87,11 +133,22 @@ object Deps {
         const val glide = "com.github.bumptech.glide:glide:4.12.0"
         const val coil = "io.coil-kt:coil-compose:2.2.0"
     }
+
+    object DataStore {
+        const val datastore = "androidx.datastore:datastore:1.0.0"
+    }
+
+    object Mockito {
+        const val core = "org.mockito:mockito-core:5.3.1"
+        const val mockitoKotlin = "org.mockito.kotlin:mockito-kotlin:5.0.0"
+        const val mocc = "io.mockk:mockk:1.13.5"
+    }
 }
 
 private typealias pdss = PluginDependenciesSpec
 private typealias pds = PluginDependencySpec
 
+inline val pdss.androidApplication: pds get() = id("com.android.application")
 inline val pdss.androidLibrary: pds get() = id("com.android.library")
 inline val pdss.kotlin: pds get() = id("org.jetbrains.kotlin.android")
 inline val pdss.parcelize: pds get() = id("kotlin-parcelize")
