306224a0f858c58d88bc0b13b15ebdaa755d01e9
Podzorov Eugene <p.eugenics@gmail.com>
2023-08-08 17:19:54 +0700

    version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
 
 android {
     namespace = "com.eugenics.freeradio"
-    compileSdk = BaseConfig.compileSdk
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = BaseConfig.applicationId
-        minSdk = BaseConfig.minSdk
-        targetSdk = BaseConfig.targetSdk
-        versionCode = BaseConfig.versionCode
-        versionName = BaseConfig.versionName
+        applicationId = libs.versions.applicationId.get()
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        versionCode = libs.versions.versionCode.get().toInt()
+        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "com.eugenics.core_testing.HiltTestRunner"
     }
